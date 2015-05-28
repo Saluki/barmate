6893f4f1fd6f046c09e6007d98adf03db8327d18
Saluki <saluki@gmx.fr>
2015-05-28 18:50:24 +0200

    Belongs to commit previous commit (https://github.com/Saluki/Barmate/commit/57d5ca64e4e57987842b709d26284af4b0fa4897). Wrong syncing...

---
+++ app/Http/Middleware/Manager.php
@@ -0,0 +1,32 @@
+<?php namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Auth\Guard;
+
+class Manager {
+
+    protected $auth;
+
+    public function __construct(Guard $auth)
+    {
+        $this->auth = $auth;
+    }
+
+	public function handle($request, Closure $next)
+	{
+        if ($this->auth->guest() || (session('role')!=='MNGR' && session('role')!=='ADMN') )
+        {
+            if ($request->ajax())
+            {
+                return response('Unauthorized.', 401);
+            }
+            else
+            {
+                return redirect()->guest('login');
+            }
+        }
+
+		return $next($request);
+	}
+
+}
